1796c3db7e064b20e81ec66258a3d35f4932c68b
lala <2665687186@qq.com>
2019-09-11 02:36:22 +0800

    增加功能：房源基本信息填充

--- src/main/java/com/lala/service/impl/HouseServiceImpl.java
+++ src/main/java/com/lala/service/impl/HouseServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lala.service.impl;
 
+import com.lala.entity.House;
 import com.lala.entity.HouseDetail;
 import com.lala.entity.Subway;
 import com.lala.entity.SubwayStation;
@@ -9,6 +10,7 @@ import com.lala.mapper.SubwayStationMapper;
 import com.lala.service.HouseService;
 import com.lala.service.result.ServiceResult;
 import com.lala.web.form.HouseForm;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,13 +24,22 @@ public class HouseServiceImpl implements HouseService {
     SubwayMapper subwayMapper;
     @Autowired
     SubwayStationMapper subwayStationMapper;
+    @Autowired
+    ModelMapper modelMapper;
     @Override
     public void save(HouseForm houseForm) {
+        HouseDetail houseDetail = FillinDetailInfo(houseForm);
+
+        House house = new House();
+        modelMapper.map(houseForm, house);
+
+
 
     }
 
     /** 房源详细信息对象填充 **/
-    private HouseDetail FillinDetailInfo(HouseForm houseForm, HouseDetail houseDetail) {
+    private HouseDetail FillinDetailInfo(HouseForm houseForm) {
+        HouseDetail houseDetail = new HouseDetail();
         Subway subway = subwayMapper.findBySubwayId(houseForm.getSubwayLineId());
         SubwayStation subwayStation = subwayStationMapper.findBySubwayStationId(houseForm.getSubwayStationId());
         houseDetail.setDescription(houseForm.getDescription());
