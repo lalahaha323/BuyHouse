d29cb8d33c8e7ce5a4fa4aaa76bbe27546ef9cdd
lala <2665687186@qq.com>
2019-10-25 00:14:08 +0800

    修复bug：/index没有匹配的页面，报错，修复这个bug

--- src/main/java/com/lala/web/controller/HomeController.java
+++ src/main/java/com/lala/web/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
         model.addAttribute("name", "lala");
         return "index";
