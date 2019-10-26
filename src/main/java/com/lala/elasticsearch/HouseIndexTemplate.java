231366d0321e87857c6b3d9db564819d711aab59
lala <2665687186@qq.com>
2019-10-26 11:22:05 +0800

    添加功能：添加一些之后要用的elasticsearch的一些基本类

---
+++ src/main/java/com/lala/elasticsearch/HouseIndexTemplate.java
@@ -0,0 +1,34 @@
+package com.lala.elasticsearch;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 索引结构模板
+ * @author lala
+ */
+@Data
+public class HouseIndexTemplate {
+    private Long houseId;
+    private String title;
+    private int price;
+    private int area;
+    private Date createTime;
+    private Date lastUpdateTime;
+    private String cityEnName;
+    private String regionEnName;
+    private int direction;
+    private int distanceToSubWay;
+    private String subwayLineName;
+    private String subwayStationName;
+    private String street;
+    private String district;
+    private String description;
+    private String layoutDesc;
+    private String traffic;
+    private String roundService;
+    private int rentWay;
+    private List<String> tags;
+}
